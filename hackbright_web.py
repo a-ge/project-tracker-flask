b2469a0fc5a4e3b94be17f83e4666442cc38db17
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-30 16:27:50 -0800

    Added routes and templates to show, add and search for a student.

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,11 +11,47 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github)
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add-form")
+def get_student_add_form():
+    """Add a student."""
+
+    return render_template("student_add_form.html") 
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first_name = request.form.get('fname')
+    last_name = request.form.get('lname')
+    github = request.form.get('github')
+
+    print(first_name, last_name, github)
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("ack.html", github=github) 
+
+
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
